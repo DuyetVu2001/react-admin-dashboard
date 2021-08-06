1d9e7a9ff6bbb8b152babcda2214edcd0773b204
DuyetVu2001 <vungocduyet24@gmail.com>
2021-08-06 21:41:39 +0700

    change 'for' to 'htmFor'

--- src/pages/Product/index.jsx
+++ src/pages/Product/index.jsx
@@ -75,7 +75,7 @@ const Product = () => {
 								src="https://images.pexels.com/photos/7156886/pexels-photo-7156886.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
 								alt=""
 							/>
-							<label for="file">
+							<label htmlFor="file">
 								<Publish className="icon" />
 							</label>
 							<input type="file" id="file" style={{ display: 'none' }} />
